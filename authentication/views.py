116af0eedefb21f05b7dbebaeb98ffc033c82444
chu <clementsamuelm@gmail.com>
2023-11-21 01:55:07 +0700

    bonus Register + Filter item

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -16,7 +19,8 @@ def login(request):
             return JsonResponse({
                 "username": user.username,
                 "status": True,
-                "message": "Login sukses!"
+                "message": "Login sukses!",
+                "id": user.id,
                 # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
             }, status=200)
         else:
@@ -47,3 +51,24 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+
+
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        # Extracting values from form data
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        new_user = User.objects.create_user(username=username, password=password)
+            
+        return JsonResponse({
+            "status": True,
+            "message": "Account created successfully!",
+            "user_id": new_user.id  # Optionally return the created user's ID
+        }, status=200)
+    
+    return JsonResponse({
+        "status": False,
+        "message": "Invalid request method."
+    }, status=405)
--- main/views.py
+++ main/views.py
@@ -50,7 +50,7 @@ def show_xml(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
-def show_json(request):
+def show_json(request,):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
